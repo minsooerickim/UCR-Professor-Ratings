e2087ceb8087024163d089d14f8468ae2e5c995b
Minsoo Kim <minsooerickim@gmail.com>
2020-12-20 22:16:54 -0800

    succesfuly retrieved ratings!

--- index.py
+++ index.py
@@ -1,25 +1,53 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://registrationssb.ucr.edu/StudentRegistrationSsb/ssb/classRegistration/classRegistration'
-r = requests.get(url)
-
-#locally saving website
-def save_html(html, path):
-    with open(path, 'wb') as f:
-        f.write(html)
-save_html(r.content, 'local_registration_html')
-
 #accessing locally saved website
 def open_html(path):
     with open(path, 'rb') as f:
         return f.read()
 html = open_html('local_registration_html')
 
-print(html)
-#soup = BeautifulSoup(r.content, 'html.parser')
+soup = BeautifulSoup(html, 'html.parser')
 
-#professors = soup.select_one('td a .email')
+#storing all names in list
+tempProfNames = []
+for professor in soup.select('.email'):
+    tempProfNames.append(professor.text)
 
-#print(professors)
+#removing duplicate names
+profNames = []
+for professor in tempProfNames:
+    if professor not in profNames:
+        profNames.append(str(professor))
+
+#getting rateMyProf links for each found professors
+try:
+    from googlesearch import search
+except:
+    print("No moduel named 'google' found")
+
+possWebsites = []
+rateMyProfWeb = []
+for prof in profNames:
+    for i in search(prof + " rate my professor", tld = "com", num = 1, start = 0, stop = 1, pause = 2):
+        possWebsites.append(i)
+    for i in possWebsites:
+        if (i.find("ratemyprofessors") and i.find("ShowRatings")):
+            rateMyProfWeb.append(i)
+            possWebsites.clear()
+        else:
+            print("results not found for " + prof)
+
+ratings = []
+for url in rateMyProfWeb:
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    ratings.append(soup.select_one('.RatingValue__Numerator-qw8sqy-2.liyUjw').text)
+
+print(rateMyProfWeb)
+print(ratings)
+
+
+
+
 
